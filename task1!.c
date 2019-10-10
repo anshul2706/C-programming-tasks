b24d566b9f02b1b3f72f35b1db7a205eb9b5dc31
TanusriBhowmick <52447992+TanusriBhowmick@users.noreply.github.com>
2019-10-10 10:50:37 +0530

    Update task1!.c

--- task1!.c
+++ task1!.c
@@ -4,9 +4,9 @@ int main()
   int age,i;
   long phone;
   char name[20];
-  printf("Enter your name\n");
+  printf("Enter name\n");
   scanf("%s\n",name);
-  printf("Enter your age and phone number\n");
+  printf("Enter age and phone number\n");
   scanf("%d %li",&age,&phone);
   for(i=0;name[i]!='\0';i++)
   {
